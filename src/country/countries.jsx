d8a955fe12ba8091de6052db6f7eef001cd34a13
Akshya20 <146994228+Akshya20@users.noreply.github.com>
2024-11-10 09:54:14 +0530

    Update countries.jsx

--- src/country/countries.jsx
+++ src/country/countries.jsx
@@ -26,7 +26,7 @@ function Countries(){
     const [countries,setcountries]=useState([])
     console.log(countries);
     useEffect(() =>{
-        fetch(API_ENDPOINT).then(res => res.json()).then(data => setcountries(data)).catch((error)=> console.log("Error fetching data: ",error));
+        fetch(API_ENDPOINT).then(res => res.json()).then(data => setcountries(data)).catch((error)=> console.error("Error fetching data: ",error));
     },[])
     return (
         <div style={{display:"flex",flexWrap:"wrap",gap:"10px"}}>
